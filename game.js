558dfdd2d6dd6b64da4bc92d58d54b3e5f27180c
riszart <22.r.d.v.c.12@gmail.com>
2023-01-25 19:48:00 -0500

    cambiando los emoji por imagenes y restructurando la funciones de movimientos

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ let elementsSize
 let enemyPosition = []
 let level = 0
 let lives = 3 
+let keyArray = true
 const playerPosition = {
     x:undefined, 
     y:undefined,
@@ -19,108 +20,129 @@ window.addEventListener('load', setCanvasSize)
 window.addEventListener('resize', setCanvasSize)
 document.addEventListener('keydown', move)
 
+const bomb = new Image
+bomb.src = 'img/Bomb_anim0001.png'
+const player = new Image
+player.src = 'img/player.png'
+const gift = new Image
+gift.src = 'img/caja-de-regalo.png'
+const start = new Image
+start.src = 'img/hogar.png'
+
+/*
 const up = document.querySelector('#up')
 const down = document.querySelector('#down')
 const left = document.querySelector('#left')
 const right = document.querySelector('#right')
-const spanLives = document.querySelector('#lives')
-const spanTime = document.querySelector('#time')
-let timeInterbval
-let startTime
+*/
 
-right.addEventListener('click', move)
+//const spanLives = document.querySelector('#lives')
+//const spanTime = document.querySelector('#time')
+//let timeInterbval
+//let startTime
+
+/*right.addEventListener('click', move)
 left.addEventListener('click', move)
 down.addEventListener('click', move)
-up.addEventListener('click', move)
+up.addEventListener('click', move)*/
 
 
 function move(event){
   //console.log(event)   // --      ver para entender
-    let nameTwo = event.key
-    let nameOne = event.srcElement.id
-    if(nameTwo=='ArrowUp'||nameTwo=='ArrowDown'||nameTwo=='ArrowLeft'||nameTwo=='ArrowRight') nameOne = ""
-    if(nameOne == "up" || nameTwo == 'ArrowUp'){
-        if(playerPosition.y > elementsSize+15){
+    let keyboardPress = event.key
+//  let nameOne = event.srcElement.id
+//  if(nameTwo=='ArrowUp'||nameTwo=='ArrowDown'||nameTwo=='ArrowLeft'||nameTwo=='ArrowRight') nameOne = ""
+    if(/*nameOne == "up" ||*/ keyboardPress == 'ArrowUp'){
+        if(playerPosition.y > 0){
             playerPosition.y -= elementsSize
             startGame()
         }
     }
-    if(nameOne == "down" || nameTwo == 'ArrowDown'){
-        if(playerPosition.y < canvasSize - elementsSize){
+    if(/*nameOne == "down" || */keyboardPress == 'ArrowDown'){
+        if(playerPosition.y < canvasSize - elementsSize*1.5){
             playerPosition.y += elementsSize
             startGame()
         }
     }
-    if(nameOne == "left" || nameTwo == 'ArrowLeft'){
-        if(playerPosition.x > elementsSize+15){
+    if(/*nameOne == "left" || */keyboardPress == 'ArrowLeft'){
+        if(playerPosition.x > 0){
             playerPosition.x -= elementsSize
             startGame()
         }
     }
-    if(nameOne == "right" || nameTwo == 'ArrowRight'){
-        if(playerPosition.x < canvasSize - elementsSize){
+    if(/*nameOne == "right" ||*/ keyboardPress == 'ArrowRight'){
+        if(playerPosition.x < canvasSize - elementsSize*1.5){
             playerPosition.x += elementsSize
             startGame()
         }
     }
 }
 
-function setCanvasSize(){
+function setCanvasSize(){   //se adacta al tamaño de pantalla
     if(window.innerHeight > window.innerWidth){
-        canvasSize = window.innerWidth * 0.8
+        canvasSize = Number((window.innerWidth * 0.9).toFixed(0))
     }
     else{
-        canvasSize = window.innerHeight * 0.8
+        canvasSize = Number((window.innerHeight * 0.9).toFixed(0))
     } 
     canvas.setAttribute('width',canvasSize)
     canvas.setAttribute('height',canvasSize)
+    console.log('tamaño del canvas',canvasSize)
     startGame()
 }
-
 function startGame(){
     const map = maps[level]
-    if(!map){
+   /* if(!map){
         gameWin()
         return // terminar la funcion para evitar hacer render
-    }
+    }*/
     const mapRows = map.trim().split('\n')
     //  .split('')  divide un string en partes de array ( \n    divide por saltos de linea)
     const mapColumn = mapRows.map(element=>element.trim().split(''))
     // .trim()  limpia el string (elimina los espacios en blanco)
+    elementsSize = Number((canvasSize/15).toFixed(0))
 
-    elementsSize = (canvasSize/10)-1
-
-    if(!startTime){
+  /*  if(!startTime){
         startTime = Date.now()
         timeInterbval = setInterval(showTime, 100)
-    }
-    game.textAlign = "end"
-    game.font =  elementsSize-5 +"px Arial"
+    }*/
+    //game.textAlign = "start"
+    //game.font =  "50px Arial"
+    //game.textBaseline =  "middle"
+    //game.fillStyle = 'red'
+    //enemyPosition = []
 
-    enemyPosition = []
     game.clearRect(0,0,canvasSize,canvasSize)
+
     mapColumn.forEach((row,indexRow) => {
-        row.forEach((col,indeceCol)=>{
-            const emoji = emojis[col]
-            const posY = elementsSize*(indexRow+1)
-            const posX = elementsSize*(indeceCol+1)
-            game.fillText(emoji,posX+15, posY)
-            if(col=='O'){
+        row.forEach((col,indexCol)=>{
+            const posY = elementsSize*(indexRow)
+            const posX = elementsSize*(indexCol)
+            if(col == 'X'){
+                game.drawImage(bomb,posX,posY,elementsSize,elementsSize)
+                if(keyArray){
+                  enemyPosition.push({x: posX, y: posY,})
+                } 
+            }
+            else if(col == 'O'){
+                game.drawImage(start,posX,posY,elementsSize,elementsSize)
+                console.log(playerPosition.x,playerPosition.y)
                 if(!playerPosition.x && !playerPosition.y){
                     playerPosition.x = posX
                     playerPosition.y = posY
                 }
-            } else if(col == 'I'){
+            }
+            else if(col == 'I'){
+                console.log(giftPosition.x,giftPosition.y)
+                game.drawImage(gift,posX,posY,elementsSize,elementsSize)
                 giftPosition.x = posX
                 giftPosition.y = posY
-            }else if(col == 'X'){
-                enemyPosition.push({
-                        x: posX,
-                        y: posY,
-                    })
             }
+            console.log(giftPosition.x,giftPosition.y)
+            console.log(playerPosition.x,playerPosition.y)
         })
-    });
+    })
+    keyArray = false
     movePlayer()
     
     /*for(let y = 1; y<=10; y++){
@@ -140,34 +162,38 @@ function startGame(){
     //  .fillText('------',x,y)     dibuja un texto 
 }
 function movePlayer(){
-    const giftColisionX = playerPosition.x.toFixed('0') == giftPosition.x.toFixed('0') 
-    const giftColisionY = playerPosition.y.toFixed('0')  == giftPosition.y.toFixed('0') 
+    const giftColisionX = playerPosition.x == giftPosition.x
+    const giftColisionY = playerPosition.y  == giftPosition.y
     const colision = giftColisionX && giftColisionY
+
+    console.log(giftColisionX,giftColisionY)
     
     if(colision){
         levelWin()
     }
     const enemyCollision = enemyPosition.find(enemy=>{
-        const enemyCollisionX = enemy.x.toFixed('3') == playerPosition.x.toFixed('3')
-        const enemyCollisionY = enemy.y.toFixed('3') == playerPosition.y.toFixed('3')
+        const enemyCollisionX = enemy.x == playerPosition.x
+        const enemyCollisionY = enemy.y == playerPosition.y
         return enemyCollisionX && enemyCollisionY
     })
     if(enemyCollision){
         levelFail()
     }
-    game.fillText(emojis['PLAYER'], playerPosition.x+15, playerPosition.y)
+    game.drawImage(player,playerPosition.x,playerPosition.y,elementsSize,elementsSize)
 }
 function levelWin(){
+    keyArray = true
     level++
+    console.log(level)
     startGame()
 }
-function gameWin(){
+//function gameWin(){
 /*  localStorage    --  funciona solo en el frontend, guarda una variable en un navegador
     no funciona como un objeto y funcionan con metodos:
     getItem --  leer un valor guardado en el localStorage del navegador (localStorage.getItem('nombre')
     setItem --  guardar en el localStorage del navegador (localStorage.setItem('nombre','valor'))
     removeItem  --  elimina la variable*/
-    clearInterval(timeInterbval)
+    /*clearInterval(timeInterbval)
     let recorTime = localStorage.getItem('record_time')
     let playerTime = Date.now() - startTime
     console.log(playerTime)
@@ -182,7 +208,7 @@ function gameWin(){
         localStorage.setItem('record_time', playerTime)
         console.log('primer ganador')
     }
-}
+}*/
 function levelFail(){
     showlives()
     lives--
@@ -199,14 +225,14 @@ function levelFail(){
 showlives()
 //      .join('')   --- une los elementos de un array con lo que establesiste en la comilla
 function showlives(){
-    /*  const heartArray = Array(lives).fill(emojis['HEART'])
-      spanLives.innerHTML = heartArray.join('')*/
-    spanLives.innerHTML = emojis['HEART'].repeat(lives)
+    //const heartArray = Array(lives).fill(emojis['HEART'])
+    //spanLives.innerHTML = heartArray.join('')
+    //spanLives.innerHTML = emojis['HEART'].repeat(lives)
 }
 //  setTimeout(()=>console.log('hola'),1000)    --  ejecuta la funcion una sola ves despues del tiempo concurrido
 //  setInterval(()=>console.log('hola'),1000)    --  ejecuta la funcion cada sierto tiempo definido
 //  clearInterval(nombre del interbalo)     --  detiene el tiempo del interbalo nombrado
 function showTime(){
-    spanTime.innerHTML = Date.now() - startTime
+    //spanTime.innerHTML = Date.now() - startTime
 }
 
